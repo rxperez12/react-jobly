162444219498c78f0469c457328f6243b57c3104
Ricardo Perez <rxperez12@gmail.com>
2024-06-05 12:01:03 -0700

    fixed link for api

--- jobly-react/src/api.js
+++ jobly-react/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
