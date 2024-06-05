e648a97cbec8c414096d13815ec0c5ff709ac5a6
Ricardo Perez <rxperez12@gmail.com>
2024-06-05 12:02:50 -0700

    added company list styling

--- jobly-react/src/CompanyList.jsx
+++ jobly-react/src/CompanyList.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import SearchForm from "./SearchForm.jsx";
 import CardList from "./CardList.jsx";
+import { TEST_COMPANIES } from "./practiceData.js";
 
 /** Company List
  *
@@ -13,12 +14,25 @@ import CardList from "./CardList.jsx";
  * RoutesList -> CompanyList -> {SearchForm, CardList}
  */
 function CompanyList() {
+  const [companies, setCompanies] = useState(""); //TODO: set initial state as all companies, need to dictate what type of search it is
+
+  const [searchParams, setSearchParams] = useState("");
+
+  function handleSearch() {
+    //make api call
+    //save it to companies
+    //render list of companies according to companies list
+  }
+
   return (
     <div>
-      <h1>Company List</h1>
-      <p>Gets api calls for companies and displays appropriate companies</p>
       <SearchForm />
-      <CardList companies={"companies"} />
+      {searchParams === "" ? (
+        <h1 className="mt-2">All Companies</h1>
+      ) : (
+        <h1> Search Results for {"include correct search param"}</h1>
+      )}
+      <CardList companies={TEST_COMPANIES} />
     </div>
   );
 }
