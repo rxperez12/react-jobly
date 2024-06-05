9172a2ea68346d5704bcd7b558449e1ba7e50bd4
Ricardo Perez <rxperez12@gmail.com>
2024-06-05 12:00:40 -0700

    added practice data

---
+++ jobly-react/src/practiceData.js
@@ -0,0 +1,33 @@
+
+const TEST_COMPANIES = [
+  {
+    name: 'Salas Group',
+    handle: 'salas-group',
+    description: 'Central whom mouth partner bring newspaper special city. Show second cost newspaper can early play.',
+    logoUrl: '/logos/logo4.png'
+  },
+  {
+    name: 'Thomas and Sons',
+    handle: 'thomas-sons',
+    description: 'Book detail scene continue. Art strategy because list two.',
+    logoUrl: '/logos/logo1.png'
+  },
+  {
+    name: 'Mejia, Scott and Ryan',
+    handle: 'mejia-scott-ryan',
+    description: 'General traditional late situation discussion dog. Before best up strategy about direction.',
+    logoUrl: '/logos/logo4.png'
+  },
+  {
+    name: "Perez-Miller",
+    handle: "perez-miller",
+    description:
+      "Space one approach wife son. Themselves give necessary follow employee return feel. Step animal doctor sign water early.",
+    logoUrl: "/logos/logo4.png",
+  }
+];
+
+const TEST_JOBS = [];
+
+
+export { TEST_COMPANIES };
