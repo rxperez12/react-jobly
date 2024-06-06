8602c779b1b063cb2bce9ce69702b2468cbaf9ee
Ricardo Perez <rxperez12@gmail.com>
2024-06-06 12:04:03 -0700

    added userstate to App

--- jobly-react/src/App.jsx
+++ jobly-react/src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesList from "./RoutesList.jsx";
 import NavBar from "./NavBar.jsx";
+import { useState } from "react";
 import "./App.css";
 
 /** Component for entire page.
  *
  * Props: none
- * State: none
+ * State: userInfo - TODO:what does this look like??
  *
  * App -> {RouterList, NavBar}
  */
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
       <BrowserRouter>
