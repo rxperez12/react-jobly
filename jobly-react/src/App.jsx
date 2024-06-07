6b979acab864c3a5aac716ff4bcca7a50ff5198e
Ricardo Perez <rxperez12@gmail.com>
2024-06-07 14:41:18 -0700

    modified functions to only update user on success

--- jobly-react/src/App.jsx
+++ jobly-react/src/App.jsx
@@ -1,98 +1,138 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesList from "./RoutesList.jsx";
 import NavBar from "./NavBar.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import JoblyApi from "./api.js";
 import userContext from "./userContext.js";
 
-import { practiceUser, practiceLogin } from "./practiceData.js";
-
-const INITIAL_STATE = { user: null, err: [] };
+const INITIAL_STATE = { user: null, errs: [] };
 
 /** Component for entire page.
  *
  * Props: none
- * State: userlike { username, firstName, lastName, email, isAdmin, jobs }
+ * State: userlike user: { username, firstName, lastName, email, isAdmin, jobs }
  *
  * App -> {RouterList, NavBar}
  */
 
 function App() {
-  const [user, setUser] = useState(INITIAL_STATE);
+  const [userData, setUserData] = useState(INITIAL_STATE);
 
-  console.log("Current user", user);
+  // Logs in existing user if token exists in local storage
+  useEffect(function logInExistingUser() {
+    async function logInTokenUser() {
+      const token = localStorage.getItem("token");
 
-  /** Handle user login, make API call and if successful update state */
+      if (token !== null) {
+        JoblyApi.resetToken(token);
+        const decodedPayload = JoblyApi.decodeTokenPayload();
+
+        try {
+          updateUser(decodedPayload.username);
+        } catch (errs) {
+          localStorage.removeItem("token");
+        }
+      }
+    }
+    logInTokenUser();
+  }, []);
+
+  console.log("Current user", userData);
+
+  /** Handle user login, make API call TODO: deconstruct form data- more explicit*/
   async function handleLogin(formData) {
-    let loginData = null;
-    try {
-      loginData = await JoblyApi.logIn(formData);
-    } catch (err) {
-      //display errors to user? maybe use context for this???
-    }
-    if (loginData !== null) {
-      let userData = await JoblyApi.getUser(formData.username);
-      setUser(() => {
-        return { ...userData, errs: [] };
-      });
-    }
+    const loginData = await JoblyApi.logIn(formData);
+
+    localStorage.setItem("token", loginData.token);
+
+    updateUser(formData.username);
   }
 
+  /** Handle user sign up, make API call and updates userData in state*/
   async function handleSignup(formData) {
-    let loginData = null;
-    try {
-      signupData = await JoblyApi.signUp(formData);
-    } catch (err) {
-      //display errors to user? maybe use context for this???
-    }
-    if (loginData !== null) {
-      let userData = await JoblyApi.getUser(formData.username);
-      setUser(() => {
-        return { ...userData, errs: [] };
-      });
-    }
+    //Make API call
+    const signupData = await JoblyApi.signUp(formData);
+
+    localStorage.setItem("token", signupData.token);
+
+    updateUser(formData.username);
   }
 
+  /** Updates userData on successful form submit */
+  async function updateUser(username) {
+    let userData = await JoblyApi.getUser(username);
+    setUserData(() => {
+      return { ...userData, errs: [] };
+    });
+  }
+
+  /** Handle user edit, make API call and if successful update state */
   async function handleEdit(formData) {
-    let loginData = null;
-    try {
-      signupData = await JoblyApi.editUser(formData);
-    } catch (err) {
-      //display errors to user? maybe use context for this???
-    }
-    if (loginData !== null) {
-      let userData = await JoblyApi.getUser(formData.username);
-      setUser(() => {
-        return { ...userData, errs: [] };
+    let editData = null;
+    let inputData = {
+      username: userData.user.username,
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+    };
+
+    //Make API call
+    editData = await JoblyApi.editUser(inputData);
+
+    //Update state on success
+    if (editData !== null) {
+      setUserData(() => {
+        return { ...editData, errs: [] };
       });
     }
   }
 
+  /** Handle user edit, make JoblyApi function call and if successful update state */
   function handleLogOut() {
     let userData = JoblyApi.logOut();
+
+    localStorage.removeItem("token");
     if (userData.user === null) {
-      setUser(() => {
+      setUserData(() => {
         return { ...userData, errs: [] };
       });
     }
   }
 
-  //TODO: context is not working, fix
+  /** Display errors as alerts */
+  function displayErrors() {
+    return (
+      <div>
+        {userData.errs.map((err) => (
+          <Alert
+            err={err}
+            key={uuid()}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  if (localStorage.getItem("token") && userData.user !== null) {
+    return <i> Loading </i>;
+  }
   return (
-    <userContext.Provider value={{ user: user?.firstName }}>
+    <userContext.Provider value={{ user: userData.user?.firstName }}>
       <div className="App">
         <BrowserRouter>
           <NavBar
-            userData={user}
+            userData={userData}
             logOut={handleLogOut}
           />
+
           <div className="container">
             <RoutesList
               handleLogin={handleLogin}
               handleSignup={handleSignup}
               handleEdit={handleEdit}
-              userData={user}
+              userData={userData}
+              updateUser={updateUser}
             />
           </div>
         </BrowserRouter>
