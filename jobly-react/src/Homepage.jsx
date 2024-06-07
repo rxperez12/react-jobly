d278eeb0095d2b2f53686c6558448660efe8b222
Ricardo Perez <rxperez12@gmail.com>
2024-06-07 14:41:57 -0700

    added user show message

--- jobly-react/src/Homepage.jsx
+++ jobly-react/src/Homepage.jsx
@@ -13,12 +13,12 @@ import userContext from "./userContext.js";
  */
 
 function Homepage() {
-  // const { user } = useContext(userContext);
-  // console.log("homepage user", user);
+  const { user } = useContext(userContext);
+
   return (
-    <div>
+    <div className="m-5">
       <h1>Hi! Welcome to Jobly</h1>
-      {/* {user && <div>Welcome {user}</div>} */}
+      {user && <div>Welcome {user}</div>}
     </div>
   );
 }
